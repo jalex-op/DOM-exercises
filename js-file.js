5a9151e7b11381401f58ff7bb5b2cdc1db42534c
jordan <jalexander9383+1@gmail.com>
2023-11-01 10:29:53 -0230

    add up to blue text

--- js-file.js
+++ js-file.js
@@ -9,4 +9,26 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
+const redText = document.createElement('p');
+redText.textContent = 'Hey I\'m red!';
+redText.classList.add('red-text');
+
+const blueText = document.createElement('h3');
+blueText.textContent = 'I\'m a blue h3!';
+blueText.classList.add('blue-text');
+
+
+// const subcontent = document.createElement('div');
+// subcontent.classList.add('subcontent');
+// content.
+
+
+
 container.appendChild(content);
+container.appendChild(redText);
+container.appendChild(blueText);
+
+
+//or could do container.append(content, redText) to make it cleaner
+
+
